c5c9bf675665cb17923a7dc8bda45e93a3c41beb
ohgun <dhrjs0128@naver.com>
2022-07-10 18:19:08 +0900

    [#5]web view raect native 함수호출

--- screens/Tab1Screen.js
+++ screens/Tab1Screen.js
@@ -12,14 +12,19 @@ function Tab1Screen({ navigation, estatesStore }) {
     // estatesStore.estatesRead();
     estatesStore.webView = webView;
   }, [estatesStore]);
-  setTimeout(() => {
-    // 화면이 그려지기 전에 호출 되면 안됨
-    webView.current.injectJavaScript(`webFunction(${Dimensions.get('window').width}, ${Dimensions.get('window').height}); true;`);
-  }, 2000);
+  // setTimeout(() => {
+  //   // 화면이 그려지기 전에 호출 되면 안됨
+  //   webView.current.injectJavaScript(`webFunction(${Dimensions.get('window').width}, ${Dimensions.get('window').height}); true;`);
+  // }, 2000);
 
   return (
     <View style={{flex: 1}}>
       <WebView
+        onMessage={event => {
+          if (event.nativeEvent.data === 'mapSize') {
+            webView.current.injectJavaScript(`webFunction(${Dimensions.get('window').width}, ${Dimensions.get('window').height}); true;`);
+          }
+        }}
         ref={webView}
         cacheEnabled={false}
         cacheMode={'LOAD_NO_CACHE'}
@@ -28,7 +33,7 @@ function Tab1Screen({ navigation, estatesStore }) {
         style={{
           width: Dimensions.get('window').width
         }}
-        injectJavaScript={`webFunction(${Dimensions.get('window').width}, ${Dimensions.get('window').height}); true;`}
+        // injectJavaScript={`webFunction(${Dimensions.get('window').width}, ${Dimensions.get('window').height}); true;`}
         // injectedJavaScript={`alert('abc'); true;`}
       ></WebView>
     </View>
